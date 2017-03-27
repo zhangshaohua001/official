542ed7f78cb5cfeef7561714c3bea35a80da33cf
zhangshaohua <zhangshaohua@xin.com>
2017-03-27 15:38:19 +0800

    上传

---
+++ common/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=official;port=3306',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
